d228a44b2319e90a2d1831c3e5d17929586bc542
Aubrian Duhayon <aubrian.duhayon@mail-ecv.fr>
2024-06-18 14:57:02 +0200

    :sparkles: Config for crendentials signin

---
+++ src/lib/auth/auth.ts
@@ -0,0 +1,38 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import type { User } from "@prisma/client";
+import type { Session } from "next-auth";
+import NextAuth from "next-auth";
+import { prisma } from "../prisma";
+import { credentialsOverrideJwt, credentialsSignInCallback, getCredentialsProvider } from "./credentials-provider";
+
+
+export const { handlers, signIn, signOut, auth } = NextAuth((req) => ({
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.AUTH_SECRET,
+  callbacks: {
+    session(params) {
+      if (params.newSession) return params.session;
+
+      const typedParams = params as unknown as {
+        session: Session;
+        user?: User;
+      };
+
+      if (!typedParams.user) return typedParams.session;
+
+      typedParams.user.passwordHash = null;
+
+      return typedParams.session;
+    },
+  },
+  events: {
+    signIn: credentialsSignInCallback(req),
+  },
+  providers: [
+    getCredentialsProvider()
+  ],
+  jwt: credentialsOverrideJwt
+}));
