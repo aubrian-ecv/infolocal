ad700312542f041f6da03dcd039f918723a94771
Dancuo-Lohan <dancuo.lohan@gmail.com>
2024-06-26 10:23:14 +0200

    :sparkles: :construction: Create view of article details.

---
+++ app/(main)/article/[articleId]/page.tsx
@@ -0,0 +1,38 @@
+@ -0,0 +1,37 @@
+import { Typography } from "@/components/ui/typography";
+import { prisma } from "@/lib/prisma";
+import type { PageParams } from "@/types/next";
+import Link from "next/link";
+export default async function RoutePage(props: PageParams<{ articleId: string }>) {
+    const article = await prisma.article.findUnique({
+        where: {
+            "id": parseInt(props.params.articleId)
+        }
+    })
+
+    console.log(article)
+    return (
+        <section className="space-y-6 w-11/12 mx-auto my-2">
+            <Link href={'/'}>
+                
+                {'< Retour'}
+            </Link>
+            {
+                article!.keywords.length > 0 &&
+                <ul className="flex flex-row gap-1">
+                    {article?.keywords.split(',').map(keyword => (
+                        <li
+                            key={keyword}
+                            className={"bg-if_darkblue text-white rounded-tl-md rounded-br-md w-max px-2 py-1 text-xs"}
+                        >
+                            {keyword}
+                        </li>
+                    ))}
+                </ul>
+            }
+            <Typography variant={"h1"}>{article?.title}</Typography>
+            <img src={article?.imageUrl} />
+            <Typography variant={"p"}>{article?.content}</Typography>
+        </section>
+    )
+}
